dfb8b0319048b407e29c95bc0cb29403b56af769
Aquarian Blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-13 17:28:19 +0000

    Update push.py

--- push.py
+++ push.py
@@ -1,69 +1,22 @@
-# Program to sort alphabetically the words form a string provided by the user
+# Program to check if a number is prime or not
 
-my_str = "Hello this Is an Example With cased letters"
+num = 407
 
+# To take input from the user
+#num = int(input("Enter a number: "))
 
-# breakdown the string into a list of words
-words = my_str.split()
-
-# sort the list
-words.sort()
-
-# display the sorted words
-
-print("The sorted words are:")
-for word in words:
-       print(word)
-
-
-       # Program to sort alphabetically the words form a string provided by the user
-
-       my_str = "Hello this Is an Example With cased letters"
-
-
-       # breakdown the string into a list of words
-       words = my_str.split()
-
-       # sort the list
-       words.sort()
-
-       # display the sorted words
-
-       print("The sorted words are:")
-       for word in words:
-              print(word)
-
-              # Program to sort alphabetically the words form a string provided by the user
-
-              my_str = "Hello this Is an Example With cased letters"
-
-
-              # breakdown the string into a list of words
-              words = my_str.split()
-
-              # sort the list
-              words.sort()
-
-              # display the sorted words
-
-              print("The sorted words are:")
-              for word in words:
-                     print(word)
-
-                     # Program to sort alphabetically the words form a string provided by the user
-
-                     my_str = "Hello this Is an Example With cased letters"
-
-
-                     # breakdown the string into a list of words
-                     words = my_str.split()
-
-                     # sort the list
-                     words.sort()
-
-                     # display the sorted words
-
-                     print("The sorted words are:")
-                     for word in words:
-                            print(word)
-                            
+# prime numbers are greater than 1
+if num > 1:
+   # check for factors
+   for i in range(2,num):
+       if (num % i) == 0:
+           print(num,"is not a prime number")
+           print(i,"times",num//i,"is",num)
+           break
+   else:
+       print(num,"is a prime number")
+       
+# if input number is less than
+# or equal to 1, it is not prime
+else:
+   print(num,"is not a prime number")
